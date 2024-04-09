3d13c8ec464565a2e874f6303dd6824254e11052
Alexis <220240@fppiramide.com>
2024-04-09 13:44:49 +0200

    s

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,7 @@ import PreguntaEstilos from './fragments/PreguntaEstilos'
 import {getPregunta} from './services/services.preguntas'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [preguntas, setPreguntas]=useState([])
-
-
-  const [pregunta, setPregunta] = useState({
-    "id": 1,
-    "pregunta": "¿Qué indica la señal de tráfico triangular con un borde rojo y un dibujo de una flecha hacia abajo?",
-    "opciones": [
-      "Prohibición de paso",
-      "Obligación de ceder el paso",
-      "Dirección obligatoria",
-      "Calle sin salida"
-    ],
-    "respuesta": 2,
-    "explicacion": "La señal triangular sginifica x, la flecha para abajo significa y",
-    "categoria": "Señales de Tráfico"
-  })
 
   useEffect(()=> recogerPreguntas,[])
 
@@ -38,7 +22,7 @@ function App() {
       ))}
       <p>Estas son con estilos</p>
       {preguntas.map((pregunta,index) => (
-        <Pregunta pregunta={pregunta} key={index+3} />
+        <PreguntaEstilos pregunta={pregunta} key={index+3} />
       ))}
     </>
   )
