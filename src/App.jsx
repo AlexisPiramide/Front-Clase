b0bbd50687df469748082ab4fd05936b048da656
Alexis <220240@fppiramide.com>
2024-04-24 12:22:56 +0200

    Fetchs Funcionan y login tiene control de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,50 @@
 import { useEffect, useState } from 'react'
 //import './App.css'
 import Pregunta from './fragments/Pregunta'
-import {getPregunta} from './services/services.preguntas'
+import ExamenBoton from './fragments/ExamenBoton'
+import { getExamenes, getExamen } from './services/services.examenes'
+import LogIn from './fragments/LogIn'
+import Examen from './fragments/Examen'
 
 function App() {
-  const [preguntas, setPreguntas]=useState([])
-  const [respuestas, setRespuestas]=useState([])
-  const [validar, setValidar]= useState(false)
-  
 
+  const [usuario, setUsuario]=useState()
+  const [error, setError]=useState()
+
+  const [examenes, setExamenes] = useState([])
+  const [examen, setExamen] = useState()
+  
+  useEffect(() => {
+    recogeExamenes(usuario)
+  }, [usuario])
+
+  const recogeExamenes = async () => {
+    const result = await getExamenes(usuario)
+    setExamenes(result)
+  }
+
+  useEffect(() => {
+    console.log(examen)
+  }, [examen])
+  /*
   useEffect(()=> recogerPreguntas,[])
 
   const recogerPreguntas = async () => {
      const result = await getPregunta()
      setPreguntas(result)
   }
-
+  */
 
 
   return (
     <>
-      {preguntas.map((pregunta,index) => (
-        <Pregunta pregunta={pregunta} validar={validar} respuestas={respuestas} setRespuestas={setRespuestas} numeroPregunta={index} key={index} />
-      ))}
-      {(respuestas.length !== preguntas.length) ? <button style={{backgroundColor: 'gray'}}>Enviar Respuestas</button> : <button onClick={() => setValidar(!validar)} disabled={validar} style={{backgroundColor: validar?'gray':'#007bff'}}>Enviar Respuestas</button>}
+      <h1>{usuario ? usuario.token : '' }</h1>
+      {usuario ? examenes.map((examenDibujar, index) => (
+          <ExamenBoton  key={index} examenDibujar={examenDibujar} setExamen={setExamen} usuario={usuario}/>
+      )) : (error ? <LogIn setError={setError} error={error} setUsuario={setUsuario}/> : <LogIn setError={setError} setUsuario={setUsuario}/>)}
+       <Examen examen={examen}/>
     </>
+   
   )
 }
 
--- src/fragments/Pregunta.jsx
+++ src/fragments/Pregunta.jsx
@@ -25,7 +25,7 @@ export default function preguntas({ pregunta, respuestas, setRespuestas, numeroP
     
     return (
         <div className="secion-pregunta">
-            <h2 className="Pregunta">{pregunta.pregunta}</h2>
+            <h2 className="Pregunta">{pregunta.texto}</h2>
             <ul>
                 {pregunta.opciones.map((opcion, index) => (
                 <BotonPregunta numeroPregunta={numeroPregunta} key={index} index={index} opcion={opcion} selecionada={selecionada} setSelecionada={setSelecionada} resultado={resultado} setRespuestas={setRespuestas} validar={validar}/>
